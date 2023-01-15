3c71899216d843cf3692cfe1af71fae7baa47540
Sebastian <s.korzeniak@gmail.com>
2023-01-15 22:58:07 +0100

    add register and auth basics

---
+++ full-stack-app/client/src/components/RegisterPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>Register</div>
+
+    <input type="text" v-model="email" name="email" placeholder="Email">
+    <input type="password" v-model="password" name="password" placeholder="Password">
+    <button @click="register">Register</button>
+  </template>
+  
+  <script>
+  import AuthService from '@/services/AuthService';
+  export default {
+    data() {
+        return {
+          email:'',
+          password:''
+            
+        }
+    },
+    methods: {
+     async register() {
+      const response = await AuthService.register({
+        email: this.email,
+        password: this.password
+      })
+        console.log(response.data)
+      },
+    }
+  }
+  </script>
+  
+  
+  <style scoped>
+ 
+  </style>
+  
--- full-stack-app/client/src/router/index.js
+++ full-stack-app/client/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import RegisterPage from '../components/RegisterPage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterPage
   },
   {
     path: '/about',
